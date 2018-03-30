7ab8d875fbe01843abb112f22f00c828006f94c6
real-sky <1024795916@qq.com>
2018-03-30 17:13:20 +0800

    test git pull

--- untitled/src/GitTest.java
+++ untitled/src/GitTest.java
@@ -17,11 +17,8 @@ public class GitTest {
 
         System.out.println("new dev");
 
-<<<<<<< HEAD
         System.out.println(" dev");
-=======
         System.out.println("change dev");
->>>>>>> 77835ddc6e9ba8efe720b6e9f475fcdc8daa8d86
 
     }
 }
