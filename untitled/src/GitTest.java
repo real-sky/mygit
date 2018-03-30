edc77162156d9c6a8c7c2506c2e43cf2b69b82ba
real-sky <1024795916@qq.com>
2018-03-30 15:07:46 +0800

    git test 19 new->u

--- untitled/src/GitTest.java
+++ untitled/src/GitTest.java
@@ -6,8 +6,12 @@ public class GitTest {
         System.out.println("123");
 
         System.out.println("origin");
-        
-        System.out.println("new branch");
+
+
+
+
+
+        System.out.println("u branch");
 
     }
 }
